f4831d6990781d4360256ace0af1d5d758f54e1b
justerror <justerror@tut.by>
2016-07-27 23:44:08 +0300

    Сверстаны новости и добавлены префиксы

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-$('.main-nav__link--cabinet').click(function(){
+$('.main-nav__link--cabinet-js').click(function(){
                       event.preventDefault();
   $('.js-section-toggler').hide();
                       $('.login').show();
                       });
 
-$('.main-nav__link--registration').click(function(){
+$('.main-nav__link--registration-js').click(function(){
                       event.preventDefault();
   $('.js-section-toggler').hide();
                       $('.registration').show();
                       });
 
-$('.main-nav__link--download').click(function(){
+$('.main-nav__link--download-js').click(function(){
                       event.preventDefault();
   $('.js-section-toggler').hide();
                       $('.download').show();
                       });
 
-$('.main-nav__link--index').click(function(){
+$('.main-nav__link--index-js').click(function(){
                       event.preventDefault();
   $('.js-section-toggler').hide();
                       $('.project-news').show();
