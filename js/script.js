8c037cb761a97ed4233c359e7086b45fab248424
justerror <justerror@tut.by>
2016-07-27 11:21:35 +0300

    Сделано по первоначальному макету

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$('.main-nav__link--cabinet').click(function(){
+                      event.preventDefault();
+  $('.js-section-toggler').hide();
+                      $('.login').show();
+                      });
+
+$('.main-nav__link--registration').click(function(){
+                      event.preventDefault();
+  $('.js-section-toggler').hide();
+                      $('.registration').show();
+                      });
+
+$('.main-nav__link--download').click(function(){
+                      event.preventDefault();
+  $('.js-section-toggler').hide();
+                      $('.download').show();
+                      });
+
+$('.main-nav__link--index').click(function(){
+                      event.preventDefault();
+  $('.js-section-toggler').hide();
+                      $('.project-news').show();
+                      });
